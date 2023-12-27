63aac653f154ef3753faee54d7b889ddb2c351e3
Ashish-1505 <ashish113sharma@gmail.com>
2023-12-27 18:38:02 +0530

    first commit

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,20 +1,27 @@
-import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
+// import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import React, { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import {applyMiddleware, compose} from 'redux'
+import {createStore} from 'redux'
+import {thunk}from "redux-thunk"
+import Reducers from "./Reducers"
+ 
+const store=createStore(Reducers,compose(applyMiddleware(thunk)))
+
 
 const container = document.getElementById('root');
-const root = ReactDOM.createRoot(container);
+const root = ReactDOM.createRoot(container); 
 
 root.render(
-  <StrictMode>
-    <ColorModeScript />
-    <ChakraProvider>
-    <App />
-    </ChakraProvider>
-  </StrictMode>
+  <Provider store={store}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </Provider>
 );
 
 // If you want your app to work offline and load faster, you can change
